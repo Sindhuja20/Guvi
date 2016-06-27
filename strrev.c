80ffafff7ad02bf79c51bb07368e4757ac99d101
Sindhuja20 <sindhujasri83@gmail.com>
2016-06-27 21:32:46 +0530

    Create strrev.c

---
+++ strrev.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[20];
+printf("\n Enter the string to br reversed:");
+scanf("%s",str);
+printf("\n The reversed string is %s",strrev(str));
+return 0;
+}
